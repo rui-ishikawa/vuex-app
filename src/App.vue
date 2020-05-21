9e0912a3da3ffca8d27a60c792f359f11b65e949
rui.ishikawa <rui.ishikawa.business@gmail.com>
2020-05-21 22:24:36 +0900

    [add] カウンターサンプルコードを追加

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,27 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+  <div>
+    <p>counter: {{ counter }}</p>
+    <button @click="incrementCount()">+1</button>
+    <button @click="incrementCount(5)">+5</button>
   </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import store from './store'
+import { mapState } from "vuex"
 
-#nav {
-  padding: 30px;
+export default {
+  store,
+  name: 'App',
+  computed: {
+    // state から
+    ...mapState(['counter'])
+  },
+  methods: {
+    incrementCount(n = 1) {
+      // Actionsへ
+      this.$store.dispatch("incActionCounter", { inc: n });
+    }
+  }  
 }
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+</script>
--- src/store/index.js
+++ src/store/index.js
@@ -5,10 +5,19 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    counter : 0
   },
   mutations: {
+    incrementMutateCounter(state, payload){
+      // stateへ
+      state.counter += payload.inc
+    }    
   },
   actions: {
+    incActionCounter(store, payload){
+      // Mutationsへ
+      store.commit('incrementMutateCounter', payload)
+    }
   },
   modules: {
   }
